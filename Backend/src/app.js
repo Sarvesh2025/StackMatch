7373b670970ad1e4fcaf3bccbf75f6a62aeca170
Sarvesh2025 <utsavsarveshpandey@gmail.com>
2024-12-02 14:03:10 +0530

    Api for feed and for one user through gmail is written

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
-
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Sarvesh",
-        age: 20,
-        gender: "Male",
-        githubId: "Sarvesh2025"
-    });
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User data saved successfully");
@@ -19,6 +14,38 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("Error saving the user :" + err.message);
     }
 });
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            res.status(400).send("No user found");
+        }
+        else {
+            res.send(users);
+        }
+    }
+    catch {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// find user by emailID
+app.get("/user/:emailId", async (req,res) => {
+    try {
+        const email = req.params.emailId;
+        console.log(email);
+        const user = await User.find({ emailId: email });
+        if (user.length === 0) {
+            res.send("No user with email exist ");
+        }
+        else res.send(user);
+    }
+    catch {
+        res.status(400).send("Something went wrong");
+    }
+})
+
 connectDB().then(() => {
     console.log("Database connection established");
     app.listen(7777, () => {
