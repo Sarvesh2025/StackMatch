f749d50e50215868483139c835dd21cfb6601214
Sarvesh2025 <utsavsarveshpandey@gmail.com>
2024-12-07 15:00:26 +0530

    Api for getting all ur connections

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -25,4 +25,34 @@ userRouter.get("/requests/received", userAuth, async (req, res) => {
 
 });
 
+userRouter.get("/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find(
+            {
+                $or: [
+                    {
+                        toUserID: loggedInUser._id, status: "accepted"
+                    },
+                    { fromUserId: loggedInUser._id, status: "accepted" },
+                    
+                ],
+            }).populate("fromUserId", SAFE_DATA)
+            .populate("toUserId", SAFE_DATA);
+        
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId.id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+    
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+}
+);
+
 module.exports = userRouter;
